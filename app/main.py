8917bf454229f0938e9db98d38e7538efe2c1c90
CL0CK <humer7@icloud.com>
2026-05-19 17:44:35 +0200

    fix: correct Render API URL in frontend

--- app/main.py
+++ app/main.py
@@ -90,7 +90,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://cl0ck.github.io"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
